bb85b30a2ef51aa62cfa159059732794356ed77f
GiulianaCDA <giulianaarecippo@hotmail.com>
2022-09-11 23:35:41 -0300

    adiciona interfaces

---
+++ secomp2022/src/app/interfaces.ts
@@ -0,0 +1,13 @@
+export interface Voluntario {
+    nome: string
+    curso: string
+    numero: string
+    email: string
+}
+  
+export interface Inscrito {
+    nome: string
+    curso: string
+    email: string
+    ja_participou: boolean
+}
